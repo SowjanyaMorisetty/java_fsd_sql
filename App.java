863a55299b6f9a2676397c73c7605bdd9f40a9fb
SowjanyaMorisetty <127307331+SowjanyaMorisetty@users.noreply.github.com>
2023-03-10 18:02:14 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,21 @@
+package UST_BATCHNO3.JUNIT5_TESTCASES;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int n=5;
+    	if(n%2==0)
+    	{
+        System.out.println( "even" );
+    }
+    	else
+    	{
+    		System.out.println("odd");
+    	}
+}
+}
